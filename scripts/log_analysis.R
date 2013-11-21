688105b2452f020464fa7564c9bbf343f66f24bf
Madelaine Gogol <madelaine@gmail.com>
2013-11-21 10:35:25 -0600

    added R script to visualize log files

--- scripts/log_analysis.R
+++ scripts/log_analysis.R
@@ -2,7 +2,13 @@ setwd("/n/ngs/runs/log")
 
 #install.packages("lubridate",repos="http://cran.us.r-project.org")
 
+library(lubridate)
+library(yaml)
+
 logfiles<-system("ls -tr *.log",intern=T)
+fcids<-gsub(".log","",logfiles)
+
+
 
 logfile<-list()
 data<-list()
@@ -13,6 +19,34 @@ for(i in 1:length(logfiles))
 {
 	logfile[[i]]<-read.table(logfiles[i],sep='\t',as.is=T)
 
+	flowcell_dir<-system(paste("find /n/ngs/data -maxdepth 1 -type d -name \"*",fcids[i],"\"",sep=''),intern=T)
+	if(file.exists(paste(flowcell_dir,"/flowcell_info.yaml",sep='')))
+	{
+		flowcell_file<-paste(flowcell_dir,"/flowcell_info.yaml",sep='')
+		flowcell_info<-yaml.load_file(flowcell_file)
+
+		prot<-flowcell_info[[2]][[1]]$`:protocol`
+
+		seen_genomes<-vector()
+
+		for(j in 1:length(flowcell_info[[2]]))
+		{
+			cur_genome<-flowcell_info[[2]][[j]]$`:genome`
+			if(length(grep(cur_genome,seen_genomes))>0)
+			{
+			}else
+			{
+				seen_genomes<-c(seen_genomes,cur_genome)
+			}
+		}
+
+		flowcell_info[[2]][[1]]$`:genome`
+	}else
+	{
+		seen_genomes<-NA
+		prot<-NA
+	}
+
 	#gave up on rjson and RJSONIO because no quotes...
 	temp<-gsub("[\\{\\}]","",logfile[[i]][,1])
 
@@ -21,13 +55,13 @@ for(i in 1:length(logfiles))
 	data[[i]]<-temp
 
 	firsttime<-temp[1,3]
-	reltimes<-(temp[,3]-temp[1,3])/3600 #hours
+	reltimes<-difftime(temp[,3],temp[1,3],units="hours")
 
 	totaltime=round(max(reltimes))
 	totaltimes[i]<-totaltime
 
 	par(mar=c(5,9,3,1),oma=c(1,0,1,1))
-	dotchart(rev(as.matrix(reltimes)),labels=rev(temp[,2]),groups=factor(rep("",times=length(reltimes))),main=logfiles[i],xlab=paste("Hours (total duration:",totaltime,")",sep=''))
+	dotchart(rev(as.matrix(reltimes)),labels=rev(temp[,2]),groups=factor(rep("",times=length(reltimes))),main=paste(fcids[i],paste(seen_genomes,collapse=","),prot),xlab=paste("Hours (total duration:",totaltime,")",sep=''))
 }
 dev.off()
 
@@ -37,3 +71,15 @@ par(mar=c(10,3,3,1))
 cols<-rep("gray",times=length(logfiles))
 cols[mcm.iv]<-'blue'
 barplot(totaltimes,names.arg=logfiles,las=2,cex.names=.8,col=cols)
+
+for(i in 1:length(data))
+{
+	startpos<-min(grep("starting",data[[i]][,2]))
+	endpos<-min(grep("postrun done",data[[i]][,2]))
+
+	if(is.finite(startpos) & is.finite(endpos))
+	{
+		dur<-difftime(data[[i]][endpos,3],data[[i]][startpos,3],units="hours")
+		cat(i,dur,"\n")
+	}
+}
