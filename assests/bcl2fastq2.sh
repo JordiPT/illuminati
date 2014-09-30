dbdeda2faebd9ca79a7b13ad1f34cb91a731d7a8
Meier <srm@m00235sdnr.sgc.loc>
2014-09-30 11:59:15 -0500

    Fixed path error in qsub for bcl2fastq2

--- assests/bcl2fastq2.sh
+++ assests/bcl2fastq2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N ${JOB_NAME}
 #$ -V -j y -pe by_node ${SGE_PROC}
-#$ -cwd -b y -v PATH ${PATH}
+#$ -cwd -b y -v PATH=${PATH}
 
 # run bcl2fastq, if SGE_PROC
 ${BCL2FASTQ2} --ignore-missing-bcls --barcode-mismatches 1 \
