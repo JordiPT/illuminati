f35ae19db05fbbbf87a51e01a57f7ee7451dbd5c
Madelaine Gogol <madelaine@gmail.com>
2014-12-30 11:22:32 -0600

    beefing up checking for weird things

--- scripts/check.rb
+++ scripts/check.rb
@@ -32,43 +32,50 @@ def check_barcodes flowcell_data
 	rtn = true
 	single = false
 	custom = false
+	dual_lanes = []
 	custom_lanes = []
 
 	if flowcell_data['samples'].size < 1
 		puts red('flowcell not found')
-	end
-
-	flowcell_data['samples'].each do |sample|
-		if sample['indexSequences']
-			if sample['indexSequences'] == [""]
-				puts cyan("indexSequences is blank, lane #{sample['laneID']}. Verify that this was intentional in lims and it should be okay.")
-			end
-			if sample['indexSequences'].size > 1
-				rtn = false
-			else
-				single = true
-			end
-			
-		else
-			 puts cyan("no indexes, lane #{sample['laneID']}. Verify that this was intentional in lims and it should be okay.")
-		end
-
-		if sample['indexType'] == "Single Custom"
-			custom = true
-			custom_lanes << sample['laneID']
-		end
-	end
-
-	if custom
-		puts red("indexType is single custom, lane #{custom_lanes.uniq.join(",")}. Pipeline doesn\'t really deal with this yet. Create custom SampleSheet.csv to run pipeline or add Indexes to lims and update external_data_lims.rb with the new type.")
+		rtn = false
 	else
-		if !rtn and single
-			puts red('mix of dual and single indexes found. Check in lims - may be a dual indexed library run as single. Modify SampleSheet.csv.')
-		else
-			if !rtn and !single
-				puts red('dual indexes found. Run startup_run.rb with --dual')
+		flowcell_data['samples'].each do |sample|
+			if sample['indexSequences']
+				if sample['indexSequences'] == [""]
+					puts cyan("indexSequences is blank, lane #{sample['laneID']}. Verify that this was intentional in lims and it should be okay.")
+				end
+				if sample['indexSequences'].size > 1
+					rtn = false
+					dual_lanes << sample['laneID']
+				else
+					single = true
+				end
+				
 			else
-				puts green("all single indexes.")
+				 puts cyan("no indexes, lane #{sample['laneID']}. Verify that this was intentional in lims and it should be okay.")
+			end
+
+			if sample['indexType'] == "Single Custom"
+				custom = true
+				custom_lanes << sample['laneID']
+			end
+			if ['ILL', 'Illumina TruSeq', 'Illumina','NEB','Rubicon R40048','Rubicon_Dual'].include? sample['indexType']
+			else
+				puts red("indexType is #{sample['indexType']}, lane #{sample['landID']}")
+			end
+		end
+
+		if custom
+			puts red("indexType is single custom, lane #{custom_lanes.uniq.join(",")}. Pipeline doesn\'t really deal with this yet. Create custom SampleSheet.csv to run pipeline or add Indexes to lims and update external_data_lims.rb with the new type.")
+		else
+			if !rtn and single
+				puts red("mix of dual and single indexes found, dual in lane #{dual_lanes.uniq.join(",")}. Might be dual indexed libraries run as single or vice versa. Modify SampleSheet.csv and configureBclToFastq --use-bases-mask.")
+			else
+				if !rtn and !single
+					puts red("dual indexes found. Run startup_run.rb #{flowcell_data['FCID']} --dual")
+				else
+					puts green("all single indexes. startup_run.rb #{flowcell_data['FCID']}")
+				end
 			end
 		end
 	end
@@ -102,7 +109,7 @@ def check_genomes flowcell_data
 		end
 	end
 	if !rtn
-		puts cyan("missing genome: #{missing_lanes.uniq.join(",")}. Species is #{species}. To run without alignment, do startup_run.rb FCID --no-align")
+		puts cyan("missing genome: #{missing_lanes.uniq.join(",")}. Species is #{species}. To run without alignment, do startup_run.rb #{flowcell_data['FCID']} --no-align")
 	else
 		puts green("genomes: #{genomes.uniq.join(",")}.")
 	end
@@ -112,14 +119,22 @@ end
 def check_machine flowcell_data
 	rtn = true
 	nextseq = false
+	hiseq = false
 	missing_lanes = []
 	flowcell_data['samples'].each do |sample|
 		if sample['readLength'] =~ /^N/
 			nextseq = true
 		end
+		if sample['readLength'] =~ /^H/
+			hiseq = true
+		end
 	end
-	if nextseq
-		puts red("Nextseq run. Do /n/ngs/tools/nextseq/illuminati/bin/startup_run.rb FCID --nextseq.")
+	if nextseq 
+		if hiseq
+			puts red("Samples are labeled both Nextseq and Hiseq. Check lims.")
+		else
+			puts red("Nextseq run. /n/ngs/tools/nextseq/illuminati/bin/startup_run.rb #{flowcell_data['FCID']} --nextseq.")
+		end
 	end
 	rtn
 end
@@ -127,17 +142,20 @@ end
 def check_paired flowcell_data
 	missing_lanes = []
 	rtn = true
-	previous = flowcell_data['samples'][0]['readType']
-	flowcell_data['samples'].each do |sample|
-		if (sample['readType'] == 'Single Read' or sample['readType'] == 'Paired Reads') and sample['readType']==previous
-			previous=sample['readType']
-		else
-			rtn = false
-			missing_lanes << sample['laneID']
+	if flowcell_data['samples'][0]
+		previous = flowcell_data['samples'][0]['readType']
+		flowcell_data['samples'].each do |sample|
+			if (sample['readType'] == 'Single Read' or sample['readType'] == 'Paired Reads') and sample['readType']==previous
+				previous=sample['readType']
+			else
+				rtn = false
+				missing_lanes << sample['laneID']
+			end
 		end
-	end
-	if !rtn
-		puts red("mixed Single Read / Paired Reads on the same flowcell, manually edit the config.txt file for lane #{missing_lanes.uniq.join(",")}.")
+		if !rtn
+			puts red("mixed Single Read / Paired Reads on the same flowcell, check lims and change use-bases-mask if needed? lane #{missing_lanes.uniq.join(",")}.")
+		end
+	else
 	end
 	rtn
 end
@@ -145,15 +163,15 @@ end
 def check_lims flowcell_id
   rtn = true
   flowcell_data = json_data_for(flowcell_id)
-  barcodes = check_barcodes(flowcell_data)
   genomes = check_genomes(flowcell_data)
+  barcodes = check_barcodes(flowcell_data)
   machine = check_machine(flowcell_data)
   paired = check_paired(flowcell_data)
-
-  if !barcodes
+  if barcodes
+  else
     rtn = false
   end
-
+  #puts flowcell_data
   rtn
 end
 
